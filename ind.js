e2cf5e7e2d50e617f80fbc4fe085b223907ae57b
Sonya <sofa.chernikova@mail.ru>
2020-12-21 22:05:03 +0300

    Task №2

--- ind.js
+++ ind.js
@@ -11,6 +11,14 @@ console.log(text);
 
 // Задание 2
 
+console.log('Задание №2')
+for (let i = 0; i <= 15; ++i) {
+    if (i % 2 == 0) {
+        console.log(i + ' - четное число')
+    } else {
+        console.log(i + ' - нечетное число')
+    }
+}
 
 // Задание 3
 
