cbc787b2ce6a853003e83d898b15b20cb0651e92
Sonya <sofa.chernikova@mail.ru>
2020-12-21 21:49:57 +0300

    Task №1

--- ind.js
+++ ind.js
@@ -1,5 +1,13 @@
 // Задание 1
 
+console.log('Задание №1')
+let sheeps = prompt('Введите кол-во овец: ');
+let text = "";
+
+for (let i = 1; i <= sheeps; i++) {
+    text = text + `${i} овечка...`;
+}
+console.log(text);
 
 // Задание 2
 
